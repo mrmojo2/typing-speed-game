de58e38a96dbe613c5789f95df2d3a86a31024fa
mrmojo2 <74127740+mrmojo2@users.noreply.github.com>
2022-02-01 23:04:43 +0545

    Add files via upload

--- typingspeed.c
+++ typingspeed.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <string.h>
 
 #define MAX_WORD_LENGTH 100
 #define MAX_WORDS 100
+#define PLAY_WORDS 35
 
 int seperator(char *input,char output[MAX_WORD_LENGTH][MAX_WORDS]){
 	int i=0,c=0;
@@ -23,23 +26,36 @@ int seperator(char *input,char output[MAX_WORD_LENGTH][MAX_WORDS]){
 
 
 void readfile(char* buffer){                                                    
-        FILE * fd=fopen("./words","r");
+        FILE * fd=fopen("words.txt","r");
 
         fseek(fd,0L,SEEK_END);													
         int filesize=ftell(fd);                                                 
         fseek(fd,0L,SEEK_SET);													
 
         int size=fread(buffer,1,filesize,fd);
-        //printf("buffer :%s\n",buffer);
 }
 
 int main(){
-	int n;
-	char words[MAX_WORD_LENGTH][MAX_WORDS],buffer[2048];
+	int n,wpm;
+	char allwords[MAX_WORD_LENGTH][MAX_WORDS],buffer[2048],randwords[MAX_WORD_LENGTH][PLAY_WORDS],input[2048],inparr[MAX_WORD_LENGTH][MAX_WORDS];
+	time_t start,stop;
 	
+	start=time(NULL);
 	readfile(buffer);
-	n=seperator(buffer,words);
-
-	//for(int i=0;i<n;i++) printf("%s ",words[i]);
-	printf("%d",n);
+	n=seperator(buffer,allwords);
+	
+	srand(time(NULL));
+	for(int i=0;i<PLAY_WORDS;i++){
+		int r=rand()%n;
+		printf("%s ",allwords[r]);
+		strcpy(randwords[i],allwords[r]);
+	}
+	
+	printf("\n");
+	fgets(input,sizeof(input),stdin);
+	stop=time(NULL);
+	wpm=(PLAY_WORDS/(stop-start))*60;
+	printf("your speed is %d wpm\n",wpm);
+	
+	
 }
